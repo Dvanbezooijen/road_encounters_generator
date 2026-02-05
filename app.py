dfb2c2633d83d92368f06aac204918ed7ba749a8
Dvanbezooijen <165149311+Dvanbezooijen@users.noreply.github.com>
2026-02-05 12:11:14 +0100

    Change input steps

--- app.py
+++ app.py
@@ -21,35 +21,35 @@ aantal_rijstroken_terug = st.number_input(
 )
 
 intensiteit_heen = st.number_input(
-    "Motorvoertuig intensiteit heen (per etmaal)", min_value=0.0, value=1000.0
+    "Motorvoertuig intensiteit heen (per etmaal)", min_value=0, value=1000
 )
 
 intensiteit_terug = st.number_input(
-    "Motorvoertuig intensiteit terug (per etmaal)", min_value=0.0, value=0.0
+    "Motorvoertuig intensiteit terug (per etmaal)", min_value=0, value=0
 )
 
 snelheid_heen = st.number_input(
-    "Snelheid motorvoertuigen heen (km/h)", min_value=0.0, value=60.0
+    "Snelheid motorvoertuigen heen (km/h)", min_value=0, value=60
 )
 
 snelheid_terug = st.number_input(
-    "Snelheid motorvoertuigen terug (km/h)", min_value=0.0, value=60.0
+    "Snelheid motorvoertuigen terug (km/h)", min_value=0, value=60
 )
 
 fiets_h = st.number_input(
-    "Fiets intensiteit heen (per etmaal)", min_value=0.0, value=20.0
+    "Fiets intensiteit heen (per etmaal)", min_value=0, value=20
 )
 
 fiets_t = st.number_input(
-    "Fiets intensiteit terug (per etmaal)", min_value=0.0, value=20.0
+    "Fiets intensiteit terug (per etmaal)", min_value=0, value=20
 )
 
 fietsSpeedKmh = st.number_input(
-    "Fiets snelheid (km/h)", min_value=0.0, value=18.0
+    "Fiets snelheid (km/h)", min_value=0, value=18
 )
 
 length_km = st.number_input(
-    "Weg lengte (km)", min_value=0.0, value=0.1
+    "Weg lengte (km)", min_value=0.0, value=1.0
 )
 
 if st.button("Compute encounters"):
@@ -69,10 +69,11 @@ if st.button("Compute encounters"):
     st.subheader("Results (per hour)")
     st.metric(
         "Total encounters",
-        f"{results['totalEncountersHour']:.2f}"
+        f"{results['totalEncountersHour']:.0f}"
     )
 
     st.write("Breakdown:")
-    st.write(f"🚗🚗 Car–Car: {results['ccEncountersHour']:.2f}")
-    st.write(f"🚲🚗 Bike–Car: {results['bcEncountersHour']:.2f}")
-    st.write(f"🚲🚲 Bike–Bike: {results['bbEncountersHour']:.2f}")
+    st.write(f"🚗🚗 Car–Car: {results['ccEncountersHour']:.0f}")
+    st.write(f"🚲🚗 Bike–Car: {results['bcEncountersHour']:.0f}")
+    st.write(f"🚲🚲 Bike–Bike: {results['bbEncountersHour']:.0f}")
+    st.write("Since the model outputs float point numbers, rounding errors may apply the the above results")
